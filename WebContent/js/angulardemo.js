fcd204b2c77d9f93b845b60747425b0a7f70bbc1
Khushboo Bharti CW <kbharti.cw@mmm.com>
2017-04-14 15:49:23 +0530

    routing template added

--- WebContent/js/angulardemo.js
+++ WebContent/js/angulardemo.js
@@ -1,6 +1,32 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ["ngRoute"]);
 
-app.controller("demoController", function($scope) {
+app.controller("mainController",["$scope",function($scope) {
+$scope.title= "my first Angularjs App";
  $scope.book = "The Alchemist";
  $scope.author = "Paulo Coelho";
-});
+}]);
+app.controller("homeController",["$scope",function($scope){
+	$scope.title="i am in home page";
+}]);
+app.controller("aboutController", ["$scope",function ($scope){
+	$scope.title="i am in about page";
+}]);
+app.controller("contactController",["$scope",function($scope){
+	$scope.title="i m in contact page";
+}]);
+app.config(function($routeProvider){
+	$routeProvider
+	.when("/", {
+		templateUrl : "pages/home.html",
+		controller :  "mainController"
+	})
+	.when("/about",{
+		templateUrl : "pages/about.html",
+		controller: "aboutController"
+	})
+	.when("/contact",{
+		templateUrl: "contact.html",
+		controller: "contactController"
+	});
+	
+})
